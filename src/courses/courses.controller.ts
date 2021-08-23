5394d6becdf67de1f3443217b5344c78bea43d94
Gabriel Reira <gabrielmfreira@gmail.com>
2021-08-22 22:36:18 -0300

    creating routes, controller and service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { 
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
-@Controller('')
+
+@Controller('courses')
 export class CoursesController {
-  @Get('courses')
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Get('')
   findAll() {
-    return 'Listagem de cursos';
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
+  }
+  // @Get(':id')
+  // findOne(@Param() params) {
+  //   return `Curso ${params.id}`;
+  // }
+
+  @Post()
+  create(@Body() body) {
+    return this.coursesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coursesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coursesService.remove(id);
   }
 }
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,68 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: "Lorem ipsum",
+      description: "Lorem ipsum dolor sit amet.",
+      tags: ["nestjs", "nodejs", "typescript"]
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    const course = this.courses.find(
+      (course: Course) => course.id === Number(id)
+    );
+
+    if (!course) {
+      throw new HttpException(
+        `Course with id ${id} not found`,
+        HttpStatus.NOT_FOUND
+      );
+    }
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const courseIndex = this.courses.findIndex(
+      (course: Course) => course.id === Number(id)
+    );
+
+    if (courseIndex >= 0) {
+      this.courses[courseIndex] = updateCourseDto;
+    }
+    else {
+      throw new HttpException(
+        `Course with id ${id} not found`,
+        HttpStatus.NOT_FOUND
+      );
+    }
+  }
+
+  remove(id: string) {
+    const courseIndex = this.courses.findIndex(
+      (course: Course) => course.id === Number(id)
+    );
+    console.log(courseIndex)
+
+    if (courseIndex >= 0) {
+      this.courses.splice(courseIndex, 1)
+    }
+    else {
+      throw new HttpException(
+        `Course with id ${id} not found`,
+        HttpStatus.NOT_FOUND
+      );
+    }
+  }
+}

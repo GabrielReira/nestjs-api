90aefa77e2ff1acfd740329c0b7f4224f9b6225e
Gabriel Reira <gabrielmfreira@gmail.com>
2021-09-15 16:40:27 -0300

    migrating to postgres db using docker

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,19 @@
 module.exports = {
-  "type": "mysql",
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASS,
-  "database": process.env.DB_NAME,
-  "entities": [
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [
     "dist/**/*.entity{.ts,.js}"
   ],
-  "autoLoadEntities": true,
-  "synchronize": true
+  migrations: [
+    'dist/src/migrations/*{.ts,.js}'
+  ],
+  cli: {
+    "migrationsDir": "src/migrations"
+  },
+  autoLoadEntities: true,
+  synchronize: false
 }
